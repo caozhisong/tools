25459cba24e4ae016b1e7427ab965d44860cd722
Jeffrey.cao <caozhisong@sina.cn>
2017-11-24 23:05:07 +0800

    add google test and lcov project

---
+++ googletest/googletest-master/googletest/obj/local/armeabi/objs/gtest/__/src/gtest-all.o.d
@@ -0,0 +1,136 @@
+/home/irdetogame/jcao/mygtest_arm/googletest-master/googletest/obj/local/armeabi/objs/gtest/__/src/gtest-all.o: \
+  /home/irdetogame/jcao/mygtest_arm/googletest-master/googletest/jni/../src/gtest-all.cc \
+  include/gtest/gtest.h \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/limits \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/ostream \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+  include/gtest/internal/gtest-internal.h \
+  include/gtest/internal/gtest-port.h \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream \
+  include/gtest/internal/gtest-port-arch.h \
+  include/gtest/internal/custom/gtest-port.h \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/tr1/tuple \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/iomanip \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/set \
+  include/gtest/gtest-message.h include/gtest/internal/gtest-string.h \
+  include/gtest/internal/gtest-filepath.h \
+  include/gtest/internal/gtest-type-util.h \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/cxxabi.h \
+  include/gtest/gtest-death-test.h \
+  include/gtest/internal/gtest-death-test-internal.h \
+  include/gtest/gtest-param-test.h \
+  include/gtest/internal/gtest-param-util.h \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/iterator \
+  include/gtest/internal/gtest-linked_ptr.h \
+  include/gtest/gtest-printers.h \
+  include/gtest/internal/custom/gtest-printers.h \
+  include/gtest/internal/gtest-param-util-generated.h \
+  include/gtest/gtest_prod.h include/gtest/gtest-test-part.h \
+  include/gtest/gtest-typed-test.h include/gtest/gtest_pred_impl.h \
+  src/gtest.cc include/gtest/internal/custom/gtest.h \
+  include/gtest/gtest-spi.h \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/list \
+  src/gtest-internal-inl.h src/gtest-death-test.cc src/gtest-filepath.cc \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+  src/gtest-port.cc \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/fstream \
+  src/gtest-printers.cc \
+  /home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/cwchar \
+  src/gtest-test-part.cc src/gtest-typed-test.cc
+
+include/gtest/gtest.h:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/limits:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/ostream:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+include/gtest/internal/gtest-internal.h:
+
+include/gtest/internal/gtest-port.h:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream:
+
+include/gtest/internal/gtest-port-arch.h:
+
+include/gtest/internal/custom/gtest-port.h:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/tr1/tuple:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/iomanip:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/set:
+
+include/gtest/gtest-message.h:
+
+include/gtest/internal/gtest-string.h:
+
+include/gtest/internal/gtest-filepath.h:
+
+include/gtest/internal/gtest-type-util.h:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/cxxabi.h:
+
+include/gtest/gtest-death-test.h:
+
+include/gtest/internal/gtest-death-test-internal.h:
+
+include/gtest/gtest-param-test.h:
+
+include/gtest/internal/gtest-param-util.h:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/iterator:
+
+include/gtest/internal/gtest-linked_ptr.h:
+
+include/gtest/gtest-printers.h:
+
+include/gtest/internal/custom/gtest-printers.h:
+
+include/gtest/internal/gtest-param-util-generated.h:
+
+include/gtest/gtest_prod.h:
+
+include/gtest/gtest-test-part.h:
+
+include/gtest/gtest-typed-test.h:
+
+include/gtest/gtest_pred_impl.h:
+
+src/gtest.cc:
+
+include/gtest/internal/custom/gtest.h:
+
+include/gtest/gtest-spi.h:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/list:
+
+src/gtest-internal-inl.h:
+
+src/gtest-death-test.cc:
+
+src/gtest-filepath.cc:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+src/gtest-port.cc:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/fstream:
+
+src/gtest-printers.cc:
+
+/home/irdetogame/opt/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/cwchar:
+
+src/gtest-test-part.cc:
+
+src/gtest-typed-test.cc:
